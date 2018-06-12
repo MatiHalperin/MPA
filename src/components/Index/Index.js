71680823fddff99bc1485cf1e2ce4452d3293ba5
Mati Halperin <matias.halperin@gmail.com>
2018-06-12 10:39:51 -0300

    Add login page and all the basic routing structure

---
+++ src/components/Index/Index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import logo from './logo.svg';
+import './Index.css';
+
+class Index extends Component {
+  render() {
+      return (
+        <div className="Index">
+          <header className="Index-header">
+            <img src={logo} className="Index-logo" alt="logo" />
+            <h1 className="Index-title">Welcome to React</h1>
+          </header>
+          <p className="Index-intro">
+            <Link to="/login">Login</Link>
+          </p>
+        </div>
+      );
+  }
+}
+
+export default Index;

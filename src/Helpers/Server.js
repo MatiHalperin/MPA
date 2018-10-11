32a1502a2159c1ba1b10aa1b15e6ca45d423f75c
Mati Halperin <matias.halperin@gmail.com>
2018-10-11 09:02:58 -0300

    Add new interact method to server to avoid duplicated code

--- src/Helpers/Server.js
+++ src/Helpers/Server.js
@@ -1,6 +1,27 @@
 class Server {
   static getURL() {
-    return "http://10.10.5.13:3000";
+    return "http://10.10.1.6:3000";
+  }
+
+  static async interact(method, path, body) {
+    try {
+      let requestInit = {
+        method: method,
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json",
+        }
+      }
+
+      if (body)
+        requestInit.body = body;
+
+      let response = await fetch(Server.getURL() + path, requestInit);
+      let responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   static async getJson(path) {
